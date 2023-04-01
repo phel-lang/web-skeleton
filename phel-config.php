7e22da591f2f487e4d658431e5f341df31dd2bee
Chemaclass <chemaclass@outlook.es>
2023-04-01 12:24:34 +0200

    Update phel:0.10 and router:0.1

--- phel-config.php
+++ phel-config.php
@@ -2,14 +2,18 @@
 
 declare(strict_types=1);
 
-return [
-    'src-dirs' => ['src'],
-    'test-dirs' => ['tests'],
-    'vendor-dir' => 'vendor',
-    'out-dir' => 'out',
-    'export' => [
-        'directories' => ['src/modules'],
-        'namespace-prefix' => 'PhelGenerated',
-        'target-directory' => 'src/PhelGenerated',
-    ],
-];
+use Phel\Config\PhelConfig;
+use Phel\Config\PhelExportConfig;
+
+return (new PhelConfig())
+    ->setSrcDirs(['src'])
+    ->setTestDirs(['tests'])
+    ->setVendorDir('vendor')
+    ->setOutDir('out')
+    ->setExport((new PhelExportConfig())
+        ->setDirectories(['src/phel'])
+        ->setNamespacePrefix('PhelGenerated')
+        ->setTargetDirectory('src/PhelGenerated'))
+    ->setIgnoreWhenBuilding(['src/local.phel'])
+    ->setKeepGeneratedTempFiles(false)
+    ->setFormatDirs(['src', 'tests']);
