3838424882b0c3f348aab57507f506c09af42be3
Chemaclass <chemaclass@outlook.es>
2024-03-16 16:48:31 +0100

    simplify run:dev and run:prod

--- public/index.php
+++ public/index.php
@@ -9,8 +9,7 @@ require $projectRootDir . '/vendor/autoload.php';
 
 $appFile = $projectRootDir . "/out/index.php";
 
-$phelMode = getenv('PHEL_MODE') ?: 'dev';
-if ($phelMode === 'prod' && file_exists($appFile)) {
+if (file_exists($appFile)) {
     require_once $appFile;
 } else {
     Phel::run($projectRootDir, 'web-skeleton\app');
